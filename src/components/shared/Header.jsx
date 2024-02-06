fc9e148ab610f21080581c0af3ba32c272a371e2
Sandaru  De Silva <sandarualoka99@gmail.com>
2024-02-06 10:20:13 +0530

    adding Logo and sidebar

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { CiSearch } from "react-icons/ci";
 
 export const Header = () => {
   return (
     <div className="bg-white h-16 px-4 flex justify-between items-center">
-      <div>
+      <div className="relative">
+        <CiSearch fontSize={28} className="text-gray-400 absolute mt-2 pl-3" />
+
         <input
           type="text"
-          placeholder="Search here.."
-          className="text-sm focus:outline-none active:outline-none"
+          placeholder="Searchs here.."
+          className="text-sm focus:outline-none active:outline-none h-10 w-[24rem] border border-gray-300 rounded-lg pl-10 px-4"
         />
       </div>
       <div>Menus</div>
--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import classNames from "classnames";
-import { FcBullish } from "react-icons/fc";
+// import { FcBullish } from "react-icons/fc";
 import { HiOutlineLogout } from "react-icons/hi";
 import {
   DASHBOARD_SIDEBAR_LINKS,
   DASHBOARD_SIDEBAR_BOTTOM_LINKS,
 } from "../../lib/consts/navigation";
+import Logo from "../../assets/Logo.png";
 
 const linkClass =
   "flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base";
@@ -15,8 +16,8 @@ export default function Sidebar() {
   return (
     <div className="bg-neutral-900 w-60 p-3 flex flex-col">
       <div className="flex items-center gap-2 px-1 py-3">
-        <FcBullish fontSize={24} />
-        <span className="text-neutral-200 text-lg">ERP</span>
+        {/* <FcBullish fontSize={24} /> */}
+        <img src={Logo} alt="Logo" className="h-[150px] w-[150px]" />
       </div>
       <div className="py-8 flex flex-1 flex-col gap-0.5">
         {DASHBOARD_SIDEBAR_LINKS.map((link) => (
